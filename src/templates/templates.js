8a036e8d6d241919c1b1c3f8b2fd7b9bfeb39e50
Niall <niall.deighan@injixo.com>
2015-03-03 09:35:29 +0000

    Modified blockslider control colour.

--- src/templates/templates.js
+++ src/templates/templates.js
@@ -2196,27 +2196,50 @@ function program3(depth0,data) {
 this["Handlebars"]["templates"]["tutor"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression, self=this;
+  var buffer = "", stack1, helper, functionType="function", self=this;
 
 function program1(depth0,data) {
   
-  var buffer = "", stack1;
-  buffer += "\n            <audio src=\""
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0._media)),stack1 == null || stack1 === false ? stack1 : stack1.mp3)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\" type=\"audio/mp3\" autoplay />\n                ";
+  var buffer = "", stack1, helper;
+  buffer += "\n            <audio src=\"";
+  if (helper = helpers.feedbackAudioMp3) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.feedbackAudioMp3); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\" type=\"audio/mp3\" autoplay />\n                ";
   return buffer;
   }
 
 function program3(depth0,data) {
   
-  var buffer = "", stack1;
-  buffer += "\n            <audio src=\""
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0._media)),stack1 == null || stack1 === false ? stack1 : stack1.ogg)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\" type=\"audio/ogg\" autoplay />\n                ";
+  var buffer = "", stack1, helper;
+  buffer += "\n            <audio src=\"";
+  if (helper = helpers.feedbackAudioOgg) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.feedbackAudioOgg); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\" type=\"audio/ogg\" autoplay />\n                ";
   return buffer;
   }
 
 function program5(depth0,data) {
+  
+  var buffer = "", stack1, helper;
+  buffer += "\n        <div class=\"tutor-image\">\n                <img src=\"";
+  if (helper = helpers.feedbackGraphic) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.feedbackGraphic); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\" alt=\"";
+  if (helper = helpers.feedbackGraphicAlt) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.feedbackGraphicAlt); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\" title=\"";
+  if (helper = helpers.feedbackGraphicTitle) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.feedbackGraphicTitle); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\"/>\n        </div>\n		";
+  return buffer;
+  }
+
+function program7(depth0,data) {
   
   var buffer = "", stack1, helper;
   buffer += "\n                <div class=\"tutor-title\">\n                    <h5 class=\"tutor-title-inner\">\n                    ";
@@ -2228,17 +2251,13 @@ function program5(depth0,data) {
   }
 
   buffer += "\n<div class=\"tutor\">\n    \n    <div class=\"tutor-toolbar\">\n        <a href=\"#\" class=\"tutor-done\">\n            <div class=\"tutor-icon-close icon icon-cross\"></div>\n        </a>\n    </div>\n    <div class=\"tutor-content\">\n            <div class=\"tutor-audio\">\n                ";
-  stack1 = helpers['if'].call(depth0, ((stack1 = (depth0 && depth0._media)),stack1 == null || stack1 === false ? stack1 : stack1.mp3), {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.feedbackAudioMp3), {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n            </div>\n        <div class=\"tutor-image\">\n                <img src=\""
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0._graphic)),stack1 == null || stack1 === false ? stack1 : stack1.src)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\" alt=\""
-    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0._graphic)),stack1 == null || stack1 === false ? stack1 : stack1.alt)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "\" title=\"";
-  stack1 = ((stack1 = ((stack1 = (depth0 && depth0._graphic)),stack1 == null || stack1 === false ? stack1 : stack1.title)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1);
+  buffer += "\n            </div>\n			\n		";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.feedbackGraphic), {hash:{},inverse:self.noop,fn:self.program(5, program5, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\"/>\n        </div>\n        <div class=\"tutor-inner\">\n                ";
-  stack1 = helpers['if'].call(depth0, (depth0 && depth0.feedbackTitle), {hash:{},inverse:self.noop,fn:self.program(5, program5, data),data:data});
+  buffer += "\n        <div class=\"tutor-inner\">\n                ";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.feedbackTitle), {hash:{},inverse:self.noop,fn:self.program(7, program7, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n            <div class=\"tutor-body\">\n                <div class=\"tutor-body-inner\">";
   if (helper = helpers.feedbackMessage) { stack1 = helper.call(depth0, {hash:{},data:data}); }
