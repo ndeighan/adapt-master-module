f42bbca72ae0b95fcb036d20ebabae2e15c7f2e7
Niall <niall.deighan@injixo.com>
2015-04-23 12:16:18 +0100

    update to webObject.js. Attempt to get framework to pickup change in variable from embeded content.

--- src/components/adapt-webObject/js/adapt-webObject.js
+++ src/components/adapt-webObject/js/adapt-webObject.js
@@ -1,83 +1,39 @@
 /*
 * adapt-webObject
 */
+var triggerVar = "0";
+
 define(function(require) {
   var ComponentView = require('coreViews/componentView');
   var Adapt = require('coreJS/adapt');
-<<<<<<< HEAD
   
-  var triggerVar = 0;
-  
-    
-=======
-
->>>>>>> f5d6ec6fef2711e525e2d3f573a3f07c57488e27
+   
   var WebObject = ComponentView.extend({
       
     postRender: function() {
       this.setReadyStatus();
-<<<<<<< HEAD
       this.intervalCheck();
     },  
       
     intervalCheck: function() {
-        if (triggerVar == 0) {
+        if (triggerVar == "0") {
            console.log("triggerVar is still zero", triggerVar);
            triggerCheck = setInterval(this.inview1, 1000);
-        } else {
-        console.log("else has been fired", triggerVar);
-        this.inview1();
-          }
+		   this.triggerCheck;
+        } 
         
     },
       
     inview1: function() {
         console.log("trying completion", triggerVar);
-        if (triggerVar == 1) {
+        if (triggerVar == "1") {
+			console.log("trying status", triggerVar);
             this.setCompletionStatus();
-        }
-    }    
+             }
+    }        
 
   });
     
-=======
-      this.$('.component-inner').on('inview', _.bind(this.inview, this));
-    },      
-      
-    inview: function(event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
-            if (visiblePartY === 'top') {
-                this._isVisibleTop = true;
-            } else if (visiblePartY === 'bottom') {
-                this._isVisibleBottom = true;
-            } else {
-                this._isVisibleTop = true;
-                this._isVisibleBottom = true;
-            }
-
-            if (this._isVisibleTop && this._isVisibleBottom) {
-                this.$('.component-inner').off('inview');
-                this.setCompletionStatus();
-            }
-            
-        }
-    },
-      
-  });
-
->>>>>>> f5d6ec6fef2711e525e2d3f573a3f07c57488e27
   Adapt.register("webObject", WebObject);
 
 });
-
-<<<<<<< HEAD
-    var abc = function() {
-//        alert("sss");    
-    };
-   
-=======
-    function abc() {
-        alert("sss");
-        WebObject.inview('event', 'visible', 'visiblePartX', 'visiblePartY');
-    }   
->>>>>>> f5d6ec6fef2711e525e2d3f573a3f07c57488e27
