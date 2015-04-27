9e4c5411d844a687d7ab6b058318ff835ba0ba95
Niall <niall.deighan@injixo.com>
2015-04-27 11:28:45 +0100

    update to webObject.js to allow multiple webObject components on the same page. triggervar reset after completion status set to 1.

--- src/components/adapt-webObject/js/adapt-webObject.js
+++ src/components/adapt-webObject/js/adapt-webObject.js
@@ -1,38 +1,44 @@
 /*
 * adapt-webObject
 */
-var triggerVar = "0";
+
 
 define(function(require) {
   var ComponentView = require('coreViews/componentView');
   var Adapt = require('coreJS/adapt');
   
+  
    
   var WebObject = ComponentView.extend({
+	  
+    events: {
+      'inview':'inview'
+    },
       
     postRender: function() {
       this.setReadyStatus();
-      this.intervalCheck(this);
     },  
-      
-    intervalCheck: function(my_webobject) {
-        if (triggerVar == "0") {
-           console.log("triggerVar is still zero", triggerVar);
-           triggerCheck = setInterval(this.inview1, 1000, my_webobject);
-		   this.triggerCheck;
-        } 
-        
+	
+
+    inview: function(event, visible) {
+            if (visible) {
+			 triggerVar = "0";
+             triggerCheck = setInterval(this.inview1, 1000, this);
+		     this.triggerCheck;
+            }
     },
+ 
       
     inview1: function(my_webobject) {
-        console.log("trying completion", triggerVar);
         if (triggerVar == "1") {
-			console.log("trying status", triggerVar);
             my_webobject.setCompletionStatus();
-             }
-    }        
+            clearInterval(this.triggerCheck);
+        }
+    }
 
   });
+
+  
     
   Adapt.register("webObject", WebObject);
 
