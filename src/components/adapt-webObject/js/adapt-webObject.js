495fc7720272c39e3aaf8cdba45094888204ef22
Niall <niall.deighan@injixo.com>
2015-07-20 10:18:12 +0100

    refactored webObject.js to remove non-functional code.

--- src/components/adapt-webObject/js/adapt-webObject.js
+++ src/components/adapt-webObject/js/adapt-webObject.js
@@ -6,40 +6,34 @@
 define(function(require) {
   var ComponentView = require('coreViews/componentView');
   var Adapt = require('coreJS/adapt');
-  
-  
-   
+
   var WebObject = ComponentView.extend({
-	  
+
     events: {
       'inview':'inview'
     },
-      
+
     postRender: function() {
       this.setReadyStatus();
-    },  
-	
-
-    inview: function(event, visible) {
-            if (visible) {
-			 triggerVar = "0";
-             triggerCheck = setInterval(this.inview1, 100, this);
-		     this.triggerCheck;
-            }
     },
- 
-      
+
+
+    inview: function(event) {
+                         triggerVar = "0";
+             triggerCheck = setInterval(this.inview1, 100, this);
+    },
+
+
     inview1: function(my_webobject) {
         if (triggerVar == "1") {
             my_webobject.setCompletionStatus();
-            clearInterval(this.triggerCheck);
         }
     }
 
   });
 
-  
-    
+
+
   Adapt.register("webObject", WebObject);
 
 });
